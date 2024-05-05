d88b2691f6f821cebf5910eeec36be82684f5f46
trancongbao <96561274+trancongbao@users.noreply.github.com>
2024-05-05 12:20:22 +0700

    Login (#3)
    Co-authored-by: bctran <bao.cong.tran@mgm-tp.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,32 @@ import DeckStudy from "./Student/MyDecks/DeckStudy/DeckStudy";
 import Student from "./Student/Student";
 import SideBar from "./Common/SideBar/SideBar";
 import "./App.scss";
-import React from "react";
+import React, {useState} from "react";
 import Teacher from "./Teacher/Teacher";
 import DeckBrowser from "./Student/MyDecks/DeckStudy/Card/Editing/Browser";
+import Login from "./Login/Login";
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [username, setUsername] = useState('')
+
   return (
     <React.Fragment>
       <BrowserRouter>
         <SideBar />
         <Routes>
-          <Route path="/" element={<Navigate to="/teacher" />} />
-          <Route path="/teacher" element={<Teacher />} >
+          {/* Routes for home/login page */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setUsername={setUsername} />} />
+
+          {/* Routes for teacher */}
+          <Route path="/teacher" element={loggedIn ? <Teacher /> : <Navigate to="/login" />} >
             <Route path="my-decks" element={<MyDecks isTeacher={true} />} />
             <Route index element={<Navigate to="my-decks" />} />
             <Route end path={`my-decks/:deckId`} element={<DeckBrowser />} />
           </Route>
+
+          {/* Routes for teacher */}
           <Route path="/student" element={<Student />}>
             <Route path="my-decks" element={<MyDecks isTeacher={false} />} />
             <Route index element={<Navigate to="my-decks" />} />
---
+++ src/Login/Login.jsx
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+export default function Login(props) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  const navigate = useNavigate()
+
+  const login = () => {
+    fetch('http://localhost:3000/auth', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        method: "login",
+        params: {
+          userType: "teacher",
+          username: username,
+          password: password
+        }
+      }),
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.result) {
+        console.log(response.result)
+        // localStorage.setItem('user', JSON.stringify({username, token: response.token}))
+        props.setLoggedIn(true)
+        // props.setUsername(username)
+        navigate('/teacher')
+      } else {
+        window.alert('Wrong email or password')
+      }
+    })
+  }
+
+  return (
+    <div className={'mainContainer'}>
+      <div className={'inputContainer'}>
+        <input
+          value={username}
+          placeholder="Enter your username here"
+          onChange={(event) => setUsername(event.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br/>
+      <div className={'inputContainer'}>
+        <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br/>
+      <div className={'inputContainer'}>
+        <input className={'inputButton'} type="button" onClick={login} value={'Log in'}/>
+      </div>
+    </div>
+  )
+}
