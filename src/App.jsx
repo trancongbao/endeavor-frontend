f7b1a4633d9be973b48a8851a931a3e64a0665ea
trancongbao <96561274+trancongbao@users.noreply.github.com>
2024-05-10 16:03:48 +0700

    Refactor Login Routing (#16)
    Co-authored-by: bctran <bao.cong.tran@mgm-tp.com>

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,36 @@ import Login from "./Login/Login";
 import {rpc} from "./rpc/rpc";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(null)
-  const [user, setUser] = useState('')
+  /**
+   * `authenticatedUser` equals `null` means authentication has not been checked
+   * `authenticatedUser` equals `{}` means there is no authenticated session
+   *  if there is a session, `authenticatedUser` is set to the authenticated user
+   * */
+  const [authenticatedUser, setAuthenticatedUser] = useState(null)
 
   useEffect(() => {
-    rpc("auth", "currentUser", {})
-    .then((user) => {
-      if (user) {
-        setUser(user)
-        setIsLoggedIn(true)
-      } else {
-        setIsLoggedIn(false)
-      }
-    })
+    rpc("auth", "currentUser", {}).then((user) =>
+      setAuthenticatedUser(user)
+    )
   }, []);
 
-  if (isLoggedIn === null) {
-    return <div>Authentication check...</div>; //Render a loading screen until authentication is checked
+  if (authenticatedUser === null) {
+    return <div></div>; //Render an invisible screen until authentication is checked
   }
 
   //TODO: navigate to the page appropriate to the userType
   return (
     <React.Fragment>
       <BrowserRouter>
-        <SideBar user={user} setIsLoggedIn={setIsLoggedIn}/>
+        {authenticatedUser.username &&
+          <SideBar authenticatedUser={authenticatedUser} setAuthenticatedUser={setAuthenticatedUser}/>}
         <Routes>
           {/* Routes for home/login page */}
           <Route path="/" element={<Navigate to="/login"/>}/>
-          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} setUsername={setUser}/>}/>
+          <Route path="/login" element={<Login setAuthenticatedUser={setAuthenticatedUser}/>}/>
 
           {/* Routes for teacher */}
-          <Route path="/teacher" element={isLoggedIn ? <Teacher/> : <Navigate to="/login"/>}>
+          <Route path="/teacher" element={(authenticatedUser.username) ? <Teacher/> : <Navigate to="/login"/>}>
             <Route index element={<Navigate to="my-decks"/>}/>
             <Route path="my-decks" element={<MyDecks isTeacher={true}/>}/>
             <Route end path={`my-decks/:deckId`} element={<DeckBrowser/>}/>
