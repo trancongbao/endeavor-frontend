74d9361400f83d62ba9653b6d88e93566dc2b4c1
bctran <bao.cong.tran@mgm-tp.com>
2024-05-10 10:23:48 +0700

    Profile

--- src/Common/SideBar/Profile/Profile.jsx
+++ src/Common/SideBar/Profile/Profile.jsx
@@ -8,24 +8,12 @@ export default function Profile({user, setIsLoggedIn}) {
            src="https://i.seadn.io/gae/IZEEg3gIDw-zlV7Z8l1_-OgEkYKnFXJXYjEKY6Ue-Nuhfk-cv0FnRA5eOC2lM8EdEHKHNWljh_7pUmxXNtKKlzqqeZwWmNhGCoCMtew?auto=format&dpr=1&w=1000"
            alt=""/>
       <h3>{user.username}</h3>
-      <div>
-        <span>ADMIN</span>
-      </div>
-      <div>
-        {" "}
-        <span>
-          <b>Level: </b>
-        </span>
-        <span>5</span>
-      </div>
+      <div>{user.userType.toUpperCase()}</div>
+      <div>Level: 5</div>
 
       <div className="flex-btn">
-        <div className="option-btn">
-          Profile
-        </div>
-        <div className="option-btn" onClick={logout}>
-          Logout
-        </div>
+        <div className="option-btn" onClick={profile}>Profile</div>
+        <div className="option-btn" onClick={logout}>Logout</div>
       </div>
     </div>
   );
@@ -39,5 +27,9 @@ export default function Profile({user, setIsLoggedIn}) {
         alert("Logout error.")
       }
     })
+  }
+
+  function profile() {
+    //TODO
   }
 }
