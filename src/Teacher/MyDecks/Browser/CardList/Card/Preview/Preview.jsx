22f037ce3d7282c8af09e061b1b97214b89ccb26
Tran Cong Bao <trancongbao@gmail.com>
2024-06-10 14:39:12 +0700

    feature/Teacher/MyDecks/Browser/CardList/Card/Preview-and-Edit

--- src/Teacher/MyDecks/Browser/CardList/Card/Preview/Preview.jsx
+++ src/Teacher/MyDecks/Browser/CardList/Card/Preview/Preview.jsx
@@ -1,12 +1,25 @@
 import { boldNewWord } from '../../../../../../Common/Utils'
 
 export default function Preview({ card }) {
+  console.log('card: ', card)
   return (
-    <div
-      className="front-section"
-      dangerouslySetInnerHTML={{
-        __html: card ? boldNewWord(card[0].card_text) : '',
-      }}
-    ></div>
+    <>
+      <div
+        className="front-section"
+        dangerouslySetInnerHTML={{
+          __html: card ? boldNewWord(card[0].card_text) : '',
+        }}
+      ></div>
+      <hr></hr>
+      {card &&
+        card.map((word, index) => (
+          <div key={index} className="back-section item">
+            <div>
+              <span className="word bold-text">{word.word_word} </span>
+              <span className="definition">:: {word.word_definition}</span>
+            </div>
+          </div>
+        ))}
+    </>
   )
 }
